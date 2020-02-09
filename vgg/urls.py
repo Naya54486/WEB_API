0a1e0a88ae4efd375c5b1e5f413c116bad065c83
Naya54486 <you@example.com>
2020-02-09 02:43:22 -0800

    initial commit: inital settings for django rest framework

---
+++ vgg/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+# swagger not support for Django >= 3.0
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Vgg API')
+
+urlpatterns = [
+    path('', schema_view),
+    path('users/', include('users.api.urls')),
+    path('projects/', include('projects.api.urls')),
+    path('actions/', include('actions.api.urls')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
